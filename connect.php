06a63ade40ff6ed86b36380f111cc54654d8dc97
mvtindia <mvt.indiadev@outlook.com>
2016-06-04 16:39:30 +0530

    db connection change
    Changed to localhost

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
-$connect=mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna");
-// $connect=mysqli_connect("localhost","root","root","bookmyspace");
+//$connect=mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net","b660bd5f89b637","ed5c4a00","Yamuna");
+ $connect=mysqli_connect("localhost","root","root","bookmyspace");
 if($connect){
 echo "";
 }
